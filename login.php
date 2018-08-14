fead6761ef2360d5b0cd26b7a2fa9f349d6aaf19
DEEZNUTZ <36278901+AndreyCJ@users.noreply.github.com>
2018-08-14 15:22:03 +0500

    1.01

--- login.php
+++ login.php
@@ -6,12 +6,12 @@
 <div class="container">
 	<section id="showcase"><h1>Login into your Account</h1></section>
 	<form method="post" action="inc/login.inc.php">
-	<?php include("inc/errors.php"); ?>
-					<input type="text" name="uid" placeholder="Username/Email" required><br>
-					<input type="password" name="pwd" placeholder="Password" required><br>
-					<input id="reg" name="submit" type="submit" value="Login">
+		<?php include("inc/errors.php"); ?>
+		<input type="text" name="uid" placeholder="Username/Email" required><br>
+		<input type="password" name="pwd" placeholder="Password" required><br>
+		<input id="reg" name="submit" type="submit" value="Login">
 	</form>
-
+</div>
 <?php
 	include_once 'inc/footer.php';
 ?>
--- reg.php
+++ reg.php
@@ -6,17 +6,16 @@
 <div class="container">
 	<section id="showcase"><h1>Create your Account</h1></section>
 	<form method="post" action="inc/signup.inc.php">
-	<? include('inc/errors.php'); ?>
-					<input type="text" name="first" placeholder="First name*" required><br>
-					<input type="text" name="last" placeholder="Last name*" required><br>
-					<input type="email" name="email" placeholder="Email Adress*" required><br>
-					<input type="text" name="uid" placeholder="Username*" required><br>
-					<input type="password" name="pwd" placeholder="Password*" required><br>
-					<input type="password" name="cpwd" placeholder="Confirm Password*" required><br>
-			<input id="reg" type="submit" name="submit" value="Register" >
-
+		<? include('inc/errors.php'); ?>
+		<input type="text" name="first" placeholder="First name*" required><br>
+		<input type="text" name="last" placeholder="Last name*" required><br>
+		<input type="email" name="email" placeholder="Email Adress*" required><br>
+		<input type="text" name="uid" placeholder="Username*" required><br>
+		<input type="password" name="pwd" placeholder="Password*" required><br>
+		<input type="password" name="cpwd" placeholder="Confirm Password*" required><br>
+		<input id="reg" type="submit" name="submit" value="Register" >
 	</form>
-	
+</div>
 <?php
 	include_once 'inc/footer.php';
 ?>
