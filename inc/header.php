1d2efd753245acd125d7c4b8d1a57720225033a9
DEEZNUTZ <36278901+AndreyCJ@users.noreply.github.com>
2018-08-24 23:58:20 +0500

    added login sensor
    delete spaces

--- inc/header.php
+++ inc/header.php
@@ -15,7 +15,7 @@
 		<header>
 				<div id="logo"><a href="index.php"><h1>Anchous</h1></a></div>
 				<div class="spacer"></div>
-				<?php	if(isset($_SESSION['user_uid'])) : 	?>
+				<?php if(isset($_SESSION['user_uid'])) : 	?>
 					<p><a href="index.php?logout='1'" style="color:red;">Logot</a></p>
 				<?php else : ?>
 					<a href="reg.php"><input type="submit" value="Sign up"></a>
